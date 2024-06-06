2d0b3416da97623698346e18a4ff7283e56a08ba
david.thomann <david.thomann01@gmail.com>
2024-06-06 13:13:33 +0200

    first commit

---
+++ app/(tabs)/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { View, Text, StyleSheet, ScrollView } from 'react-native';
+import Server from "../../../components/Server";
+
+export default function Tab() {
+    return (
+        <View style={styles.container}>
+            <ScrollView contentContainerStyle={styles.scrollViewContent}>
+                <Server />
+                <Server />
+                <Server />
+                <Server />
+                <Server />
+                <Server />
+                <Server />
+            </ScrollView>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    scrollViewContent: {
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+});
+``
